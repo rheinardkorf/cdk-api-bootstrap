b41531a096ab28fa4c570b80415f1834e3f998a6
Rheinard Korf <rheinard.korf@xwp.co>
2021-07-25 23:20:02 +1000

    Add stack validation test to db and db-seeder.

---
+++ packages/db-seeder/test/infra.validation.test.ts
@@ -0,0 +1,11 @@
+import { SynthUtils } from "@aws-cdk/assert"
+import { Stack } from "@aws-cdk/core"
+
+import { SeederStack } from "../infra/stack"
+
+test("stack validation", () => {
+    const stack = new Stack();
+    const seeder_stack = new SeederStack(stack, 'seeder_stack');
+    const received = SynthUtils.toCloudFormation(seeder_stack);
+    expect(SynthUtils.toCloudFormation(seeder_stack)).toMatchSnapshot();
+});
---
+++ packages/db/test/infra.validation.test.ts
@@ -0,0 +1,11 @@
+import { SynthUtils } from "@aws-cdk/assert"
+import { Stack } from "@aws-cdk/core"
+
+import { ServiceStack } from "../infra/stack"
+
+test("stack validation", () => {
+    const stack = new Stack();
+    const service_stack = new ServiceStack(stack, 'service_stack');
+    const received = SynthUtils.toCloudFormation(service_stack);
+    expect(SynthUtils.toCloudFormation(service_stack)).toMatchSnapshot();
+});
